9cd6dfa13d9c34788f896d35c5f7e408a0f6cbbc
famfo <famfo@famfo.xyz>
2025-02-16 12:10:09 +0100

    soc: Add support for Exynos 3830

---
+++ include/soc/exynos3830.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2022, Ivaylo Ivanov <ivo.ivanov.ivanov1@gmail.com>
+ */
+
+#ifndef EXYNOS3830_H_
+#define EXYNOS3830_H_
+
+#define DECON_F_BASE		0x13060000
+#define HW_SW_TRIG_CONTROL	0x70
+
+#endif
