9378b3f0bfc8d531490a035cd74a658f06e26395
famfo <famfo@famfo.xyz>
2025-02-16 12:19:55 +0100

    Move initialization of board_data to the stack
    When the board_data is initialized at compile time, it is placed into
    the data section, which doesn't get relocated by the linker. The resulting
    address of the board_data that is passed to board_init lies within reserved
    memory and the device crashes.

--- main/main.c
+++ main/main.c
@@ -7,29 +7,30 @@
 #include <main.h>
 #include <string.h>
 #include <lib/debug.h>
-
-/*
- * Provide an empty board config that has
- * to be filled in the board files
- * TODO: figure out why having it in
- * the board files makes devices reboot.
- */
-struct board_data board = {
-	.name = "default",
-	.ops = {
-		[BOARD_OP_INIT] = BOARD_OP_INIT,
-		[BOARD_OP_LATE_INIT] = BOARD_OP_LATE_INIT,
-		[BOARD_OP_DRIVER_SETUP] = BOARD_OP_DRIVER_SETUP,
-	}
-};
+#include <stdint.h>
 
 void main(void* dt, void* kernel, void* ramdisk)
 {
-	/* Initialize SoC and Board specific peripherals/quirks */
-	init_board_funcs(&board);
+       /*
+       * Provide an empty board config that has
+       * to be filled in the board files
+       * TODO: figure out why having it in
+       * the board files makes devices reboot.
+       */
+       struct board_data board = {
+              .name = "default",
+              .ops = {
+                     [BOARD_OP_INIT] = BOARD_OP_INIT,
+                     [BOARD_OP_LATE_INIT] = BOARD_OP_LATE_INIT,
+                     [BOARD_OP_DRIVER_SETUP] = BOARD_OP_DRIVER_SETUP,
+              }
+       };
 
-	EXECUTE_BOARD_OP(board.ops[BOARD_OP_INIT]);
-	EXECUTE_BOARD_OP(board.ops[BOARD_OP_DRIVER_SETUP]);
+       /* Initialize SoC and Board specific peripherals/quirks */
+       init_board_funcs(&board);
+
+       EXECUTE_BOARD_OP(board.ops[BOARD_OP_INIT]);
+       EXECUTE_BOARD_OP(board.ops[BOARD_OP_DRIVER_SETUP]);
 	EXECUTE_BOARD_OP(board.ops[BOARD_OP_LATE_INIT]);
 
 	printk(-1, "             .__.____                     .___\n");
